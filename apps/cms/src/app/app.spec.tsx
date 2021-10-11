800ff6566eddf4ca0f806a9048742ecc45408d6c
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-10-11 12:13:24 +0700

    feat: init cms

--- apps/cms/src/app/app.spec.tsx
+++ apps/cms/src/app/app.spec.tsx
@@ -4,14 +4,6 @@ import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-
-    expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-
-    expect(getByText('Welcome to cms!')).toBeTruthy();
+    expect(<App />);
   });
 });

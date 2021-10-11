2440eddb2558491fef43eb99a29e42a8cc71b40f
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-10-11 12:14:11 +0700

    feat: create components lib

---
+++ libs/cms/components/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'cms-components',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/cms/components',
+};

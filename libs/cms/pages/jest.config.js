118bddafb71c8beaf12f39bd82e556893c4caa5d
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-10-11 12:18:08 +0700

    feat: create pages lib

---
+++ libs/cms/pages/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'cms-pages',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/cms/pages',
+};

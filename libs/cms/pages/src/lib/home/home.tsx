9bf7bd40b847071bcb40e4afaa6d9e68454ca139
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-10-11 12:56:14 +0700

    feat: create home page

---
+++ libs/cms/pages/src/lib/home/home.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+import { Box, Stack, Heading } from '@chakra-ui/react';
+
+export function Home() {
+  return (
+    <Box>
+      <Stack>
+        <Heading>Homepage</Heading>
+      </Stack>
+    </Box>
+  );
+}
+
+export default Home;

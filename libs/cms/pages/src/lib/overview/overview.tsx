300e31edce653170759f692f6396f42f215d2a54
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-10-11 12:18:24 +0700

    feat: create overview page

---
+++ libs/cms/pages/src/lib/overview/overview.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Box, Stack, Heading } from '@chakra-ui/react';
+
+export const Overview: React.FC = () => {
+  return (
+    <Box>
+      <Stack>
+        <Heading>Overview</Heading>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Overview;

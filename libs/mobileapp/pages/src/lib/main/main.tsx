22921ee97dc59987df32ddb7b1c5b65702d50c6e
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-11-15 02:30:50 +0700

    feat: set route

--- libs/mobileapp/pages/src/lib/main/main.tsx
+++ libs/mobileapp/pages/src/lib/main/main.tsx
@@ -10,7 +10,7 @@ import {
 } from '@ionic/react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonReactRouter } from '@ionic/react-router';
-import { Home, Profile, ProductDetail, AddFood } from '../..';
+import { Home, Profile, ProductDetail, AddFood, UpdateProfile } from '../..';
 
 import { home, person } from 'ionicons/icons';
 
@@ -34,6 +34,11 @@ export function Main() {
                 children={<ProductDetail />}
               />
               <Route exact path="/add-food" children={<AddFood />} />
+              <Route
+                exact
+                path="/update-profile"
+                children={<UpdateProfile />}
+              />
               <Route exact path="/main">
                 <Redirect to="/home" />
               </Route>

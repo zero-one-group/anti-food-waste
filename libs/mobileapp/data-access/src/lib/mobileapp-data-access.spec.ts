5af8fd6d43d4b867ce66b974d1501a6a536f9f94
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-12-08 23:58:19 +0700

    feat: delete data access test

--- libs/mobileapp/data-access/src/lib/mobileapp-data-access.spec.ts
+++
@@ -1,7 +0,0 @@
-import { getProductFromOfficial } from './mobileapp-data-access';
-
-describe('mobileappDataAccess', () => {
-  it('should work', () => {
-    expect(getProductFromOfficial()).toEqual('mobileapp-data-access');
-  });
-});

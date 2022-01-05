5d255145e89e3bf28a3e0d436c8f0a351d6465f7
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-01-06 00:56:46 +0700

    feat: update utils

--- apps/mobileapp-api/src/helpers/utils.ts
+++ apps/mobileapp-api/src/helpers/utils.ts
@@ -1,13 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { compare } from 'bcrypt';
-
 export const prisma = new PrismaClient();
 
 export const comparePassword = (password: string, hashedPassword: string) => {
-  return new Promise<boolean>((res) => {
-    compare(password, hashedPassword, (err, same) => {
-      if (err) res(false);
-      else res(same);
-    });
-  });
+  return password === hashedPassword ? true : false;
 };

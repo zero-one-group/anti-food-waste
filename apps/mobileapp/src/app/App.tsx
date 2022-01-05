a5497431df508c29750986aeb5f27e73426f1fc3
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-01-06 00:54:27 +0700

    feat: remove unused component

--- apps/mobileapp/src/app/App.tsx
+++ apps/mobileapp/src/app/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import ViewMessage from './pages/ViewMessage';
 import { Login, Signup, Main } from '@anti-food-waste/mobileapp/pages';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,25 +22,26 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/login" component={Login} exact={true} />
-        <Route path="/signup" component={Signup} exact={true} />
-        <Route path="/main" component={Main} exact={true} />
-        <Route path="/home" component={Main} exact={true} />
-        <Route path="/profile" component={Main} exact={true} />
-        <Route path="/add-food" component={Main} exact={true} />
-        <Route path="/update-profile" component={Main} exact={true} />
-        <Route path="/all-official-food" component={Main} exact={true} />
-        <Route path="/all-unofficial-food" component={Main} exact={true} />
-        <Route path="/product-detail/:id" component={Main} exact={true} />
-        <Route path="/message/:id" component={ViewMessage} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/login" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/login" component={Login} exact={true} />
+          <Route path="/signup" component={Signup} exact={true} />
+          <Route path="/main" component={Main} exact={true} />
+          <Route path="/home" component={Main} exact={true} />
+          <Route path="/profile" component={Main} exact={true} />
+          <Route path="/add-food" component={Main} exact={true} />
+          <Route path="/update-profile" component={Main} exact={true} />
+          <Route path="/all-official-food" component={Main} exact={true} />
+          <Route path="/all-unofficial-food" component={Main} exact={true} />
+          <Route path="/product-detail/:id" component={Main} exact={true} />
+          <Route exact path="/" render={() => <Redirect to="/login" />} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;

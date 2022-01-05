a8ed38ffcbbd914c90d6ce9ff4fbeebd53aa49c0
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-01-06 00:56:31 +0700

    feat: create multer.ts for image upload

---
+++ apps/mobileapp-api/src/helpers/multer.ts
@@ -0,0 +1,22 @@
+import multer from 'fastify-multer';
+
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  // use is-svg for svg files https://github.com/sindresorhus/is-svg
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|webp|WEBP)$/)) {
+    req.fileValidationError = 'Only image files are allowed!';
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
+
+const limitsUpload = {
+  fileSize: 1000000, //15MB 15*1048576
+  files: 1,
+};
+
+export const upload = multer({
+  dest: '/uploads',
+  fileFilter: imageFilter,
+  limits: limitsUpload,
+});
